99fea9a225c98087c4e90590676daa515914649e
ege.eren <ege.eren@etstur.com>
2026-06-07 17:11:45 +0300

    commit

--- src/pages/layout/DashboardPage.tsx
+++ src/pages/layout/DashboardPage.tsx
@@ -1,32 +1,79 @@
-import { Avatar, Paper, Stack, Typography } from "@mui/material";
-
+import { Box, Paper, Stack, Typography } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
 function DashboardPage() {
   return (
-    <Paper
-      elevation={1}
+    <Box
       sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: "24px",
-        maxWidth: "152px",
-        height: "300px",
-        borderRadius: "16px",
-        border: "1px solid #E5E7EB",
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: '100vh',
+        paddingTop: '100px',
+        gap: '24px',
+        backgroundColor: '#fcfbfa',
       }}
     >
-      <Stack
+      {/*  <Paper
+        elevation={1}
         sx={{
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          gap: "16px",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '200px',
+          height: '300px',
+          borderRadius: '16px',
+          border: '1px solid #E5E7EB',
         }}
       >
-        <Avatar sx={{ width: "100px", height: "100px" }}>sa</Avatar>
-        <Typography sx={{ textAlign: "center" }}>sa</Typography>
-      </Stack>
-    </Paper>
+        <Stack
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            gap: '16px',
+          }}
+        >
+          <Avatar sx={{ width: '100px', height: '100px' }}>avatar</Avatar>
+          <Typography sx={{ textAlign: 'center' }}>restoran adı</Typography>
+        </Stack>
+      </Paper> */}
+      <Paper
+        elevation={1}
+        sx={{
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          width: '200px',
+          height: '300px',
+          borderRadius: '16px',
+          border: '1px solid #E5E7EB',
+        }}
+      >
+        <Stack
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            gap: '32px',
+          }}
+        >
+          <AddIcon sx={{ width: '32px', height: '32px', color: '#C4CDD5' }} />
+          <Typography
+            sx={{
+              fontSize: '16px',
+              fontWeight: '600',
+              color: '#454f5b',
+              fontFamily: 'Nunito Sans',
+            }}
+          >
+            Restoran Oluştur
+          </Typography>
+        </Stack>
+      </Paper>
+    </Box>
   );
 }
 
